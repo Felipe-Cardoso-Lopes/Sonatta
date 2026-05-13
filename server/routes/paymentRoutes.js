e4add8fdadf13864036eaab0c919c01c33f97e08
joaorgd <joaorobertoguedesdantas@gmail.com>
2026-05-13 09:52:21 -0300

    fix: correção do nome da pasta middlewares

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { createCheckoutSession } = require('../controllers/paymentController');
-const { protect } = require('../middleware/authMiddleware');
+const { verifyToken } = require('../middlewares/authMiddleware');
 
 // POST /api/payments/checkout
 // Apenas usuários autenticados (escolas) podem gerar um checkout
-router.post('/checkout', protect, createCheckoutSession);
+router.post('/checkout', verifyToken, createCheckoutSession);
 
 module.exports = router;
