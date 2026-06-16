4e6816500c78d8a7798600fb617f9a625922ec5b
joaorgd <joaorobertoguedesdantas@gmail.com>
2026-06-16 15:45:02 -0300

    test: testes de backend estabilizados e cobertura de cursos ampliada

---
+++ server/jest.setup.js
@@ -0,0 +1,9 @@
+process.env.NODE_ENV = 'test';
+
+process.env.JWT_SECRET = 'test-jwt-secret';
+
+process.env.SUPABASE_URL = 'https://test.supabase.co';
+
+process.env.SUPABASE_SERVICE_ROLE_KEY = 'test-service-role-key';
+
+process.env.MERCADOPAGO_ACCESS_TOKEN = 'test-mercadopago-token';
